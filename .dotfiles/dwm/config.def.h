cc108b175412b3ab25dd52e2cb21f6a6a7f59e60
hugoqdesh <gitjohan5@gmail.com>
2025-07-28 10:25:17 +0300

    dwm

--- .dotfiles/dwm/config.def.h
+++ .dotfiles/dwm/config.def.h
@@ -18,7 +18,7 @@ static const char col_cyan[]        = "#005577";
 //};
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
 
 static const Rule rules[] = {
 	/* xprop(1):
